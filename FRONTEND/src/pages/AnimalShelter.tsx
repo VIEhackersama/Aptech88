6829a038b7a0c11effed1e31bed2dcf2f7c3ac1e
vuanhp265 <vuanhp265@gmail.com>
2025-09-13 15:09:40 +0700

    vu

--- FRONTEND/src/pages/AnimalShelter.tsx
+++ FRONTEND/src/pages/AnimalShelter.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Adopt from '../components/adoption/Adopt';
 import AdoptDetail from '../components/adoption/AdoptDetail';
-import Event from '../components/adoption/Event';
-import ImageUploader from '../components/adoption/ImageUploader';
+import Events from '../components/adoption/Event';
 import Surrender from '../components/adoption/Surrender';
 
 export default function AnimalShelter() {
     return (
-        <div className='bg-white'>
-            <Adopt />
-            <AdoptDetail />
-            <Event />
-            <ImageUploader onImageUpload={function (image: string): void {
-                throw new Error('Function not implemented.');
-            } } />
-            <Surrender />
+        <div className="bg-gradient-to-r from-green-700 to-emerald-500 min-h-screen">
+            <Routes>
+                {/* Route mặc định khi vào /animalshelter */}
+                <Route index element={<Adopt />} />
+
+                <Route path="adopt" element={<Adopt />} />
+                <Route path="adopt/:id" element={<AdoptDetail />} />
+                <Route path="events" element={<Events />} />
+                <Route path="surrender" element={<Surrender />} />
+            </Routes>
         </div>
-    )
+    );
 }
